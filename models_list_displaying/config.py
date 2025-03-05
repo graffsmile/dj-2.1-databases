30cb98cf345c921c8423ff18f531ba5077b74d30
graffsmile <sevulybin@yandex.ru>
2025-03-05 17:02:38 +0500

    Add files via upload

---
+++ models_list_displaying/config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os.path
+
+# Загрузка переменных окружения из файла .env
+dotenv_path = 'config.env'
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+LOGIN = os.getenv('LOGIN')
+PASSWORD_PSQ = os.getenv('PASSWORD_PSQ')
+NAME_DB = os.getenv('NAME_DB')
