02ec72baec80e64146903e18dee8004d76c1c182
graffsmile <sevulybin@yandex.ru>
2025-03-05 17:14:20 +0500

    Update config.py

--- models_list_displaying/config.py
+++ models_list_displaying/config.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os.path
 
 # Загрузка переменных окружения из файла .env
-dotenv_path = 'config.env'
+dotenv_path = 'config-example.env'
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 LOGIN = os.getenv('LOGIN')
